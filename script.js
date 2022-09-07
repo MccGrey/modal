fad251581fe68f6fa0fb05602a3f784504f82d3a
MccGrey <chuxzzie01@yahoo.com>
2022-09-07 11:34:01 +0100

    update

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClosemodal = document.querySelector('.close-modal');
+const btnsOpenmodal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenmodal.length; i++)
+  btnsOpenmodal[i].addEventListener('click', openModal);
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnClosemodal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
